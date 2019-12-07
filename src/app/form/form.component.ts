4a86d60fe7a3035db7c005aea480381007d18ddd
SOCTELEMED\pnair <pnair@soctelemed.com>
2019-12-07 13:20:31 -0500

    Initial Setup

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-self',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.scss']
+})
+export class FormComponent  {
+    constructor() {
+    }
+}
