998c860d644597bf3105f19a83d8c1d95f4e3368
SOCTELEMED\pnair <pnair@soctelemed.com>
2019-12-07 18:12:28 -0500

    Desktop screen

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,11 +1,35 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
     selector: 'app-self',
     templateUrl: './form.component.html',
     styleUrls: ['./form.component.scss']
 })
-export class FormComponent  {
-    constructor() {
+export class FormComponent implements OnInit{
+    submissionForm: FormGroup;
+    submitAttempted: boolean;
+    constructor(private fb: FormBuilder) {
     }
+
+  ngOnInit(): void {
+      this.submissionForm = this.fb.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        address1: ['', Validators.required],
+        address2: '',
+      });
+  }
+
+  hasRequiredError(field) {
+    const control = this.submissionForm.get(field);
+    return (control.hasError('required') && (control.touched || this.submitAttempted));
+  }
+
+  onSubmit() {
+    if (this.submissionForm.invalid) {
+      this.submitAttempted = true;
+      return;
+    }
+  }
 }
